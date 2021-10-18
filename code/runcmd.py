aa6746bc9d0050b60a27c62b6b334d713e389255
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-10-18 14:34:28 +0700

    runcmd: refactored

--- code/runcmd.py
+++ code/runcmd.py
@@ -1,15 +1,12 @@
-import shlex
 import asyncio
 
-async def runcmd(cmd: str) -> tuple[str, str, int, int]:
-    args = shlex.split(cmd)
-    process = await asyncio.create_subprocess_exec(
-        *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+async def runcmd(cmd):
+    process = await asyncio.create_subprocess_shell(
+        cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
     )
     stdout, stderr = await process.communicate()
-    return (
-        stdout.decode("utf-8", "replace").strip(),
-        stderr.decode("utf-8", "replace").strip(),
-        process.returncode,
-        process.pid,
-    )
+    err = stderr.decode().strip()
+    out = stdout.decode().strip()
+    return out, err
