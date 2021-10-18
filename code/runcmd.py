f33058ec3b2c12269d36f9326be3c54f2674cc50
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-10-18 14:25:32 +0700

    add run command in terminal

---
+++ code/runcmd.py
@@ -0,0 +1,15 @@
+import shlex
+import asyncio
+
+async def runcmd(cmd: str) -> tuple[str, str, int, int]:
+    args = shlex.split(cmd)
+    process = await asyncio.create_subprocess_exec(
+        *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    return (
+        stdout.decode("utf-8", "replace").strip(),
+        stderr.decode("utf-8", "replace").strip(),
+        process.returncode,
+        process.pid,
+    )
