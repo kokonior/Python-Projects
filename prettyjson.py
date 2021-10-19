54de8cc1453881d36ee428ff9cb23b9f640411cd
lunatic0der <83530725+lunatic0der@users.noreply.github.com>
2021-10-19 14:55:20 +0700

    prettyjson: basictype2str

--- prettyjson.py
+++ prettyjson.py
@@ -5,3 +5,11 @@ def prettyjson(obj, indent=2, maxlinelength=80):
 
     items, _ = getsubitems(obj, itemkey="", islast=True,maxlinelength=maxlinelength - indent, indent=indent)
     return indentitems(items, indent, level=0)
+
+def basictype2str(obj):
+    if isinstance(obj, str):
+        return "\"" + str(obj) + "\""
+    elif isinstance(obj, bool):
+        return {True: "true", False: "false"}[obj]
+    else:
+        return str(obj)
