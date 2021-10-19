94e4ff335278acef26514bed0fc6872f305c5746
Muhammad Maulana <vanmoeljr8@gmail.com>
2021-10-19 14:30:40 +0700

    Commited Matrix File

--- code/matrix.py
+++ code/matrix.py
@@ -1,27 +1,30 @@
-# Function to take an input of string and new line character and return the (2nd) column and row of choice
-# An example is given. Note the function only work if there is the same number of elements in each row
+#FIRMANSYAH HELMI KURNIAWAN
+#TEKNIK MULTIMEDIA DAN JARINGAN
+#ALGORITMA DAN PEMROGRAMAN
+#2007421030
 
-class Matrix:
-    def __init__(self, matrix_string):
-        newList = matrix_string.split('\n')
-        self.rowList = []
-        self.columnList = []      
-        for row in range(len(newList)):
-            self.rowList.append([int(number) for number in newList[row].split()])     
-        for column in range(len(self.rowList[0])):
-            testList =[]
-            for row in range(len(self.rowList)):
-                testList.append(self.rowList[row][column])
-            self.columnList.append(testList)
-            
-    def row(self, index):
-        return self.rowList[index-1]
-    
-    def column(self, index):
-        return self.columnList[index-1]
+##31 Python Tutorial for Beginners | Working with Matrix in Python
 
-#Example
-matrix = Matrix("89 1903 3 4\n18 3 1 5\n9 4 800 5")
+from numpy import *
 
-print(matrix.column(2)) 
-print(matrix.row(2))
+arr1 = array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+
+arr2 = arr1.flatten()
+arr3 = arr1.reshape(3,4)
+arr4 = arr1.reshape(3,2,2)
+print("mengubah bentuk menjadi rata\n", arr2, "\n")
+print("mengubah bentuk 3 baris 4 kolom\n", arr3, "\n")
+print("mengubah bentuk 3 bagian 2 baris 2 kolom\n", arr4, "\n")
+
+matrix1 = matrix('1 2 3; 6 4 5; 1 6 7')
+diagonal = diagonal(matrix1)
+print("matrix normal\n", matrix1, "\n")
+print("matrix diagonal\n", diagonal, "\n")
+print("matrix minimal\n", matrix1.min(), "\n")
+print("matrix maksimal\n", matrix1.max(), "\n")
+
+matrix2 = matrix('1 2 3; 7 7 7; 1 6 7')
+matrix_kali = matrix1 * matrix2
+matrix_tambah = matrix1 + matrix2
+print("matrix kali\n", matrix_kali, "\n")    
+print("matrix tambah\n", matrix_tambah, "\n")    
