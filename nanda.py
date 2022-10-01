a46ab1c0110e769a96450c57e25ebc0bb4f83213
pengengantibaju <pengengantibaju@gmail.com>
2022-10-02 00:06:29 +0700

    nanda.py
    feat: add hat wobble
    ^--^  ^------------^
    |     |
    |     +-> Summary in present tense.
    |
    +-------> Type: chore, docs, feat, fix, refactor, style, or test.

---
+++ nanda.py
@@ -0,0 +1,9 @@
+# Memasukkan Inputan Angka
+angka1 = input('Tulis angka pertama: ')
+angka2 = input('Tulis angka kedua: ')
+
+# Mengkonversi Angka lalu Menjumlahkannya
+sum = int(angka1) + int(angka2)
+
+# Menampilkan Hasil Penjumlahan
+print('Hasil Penjumlahan {0} dan {1} adalah {2}'.format(angka1, angka2, sum))
