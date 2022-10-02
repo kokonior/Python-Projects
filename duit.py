b175303a1f87c687cf2f94a6d14740184d68ec4f
AgusXD <57493908+agusydk3@users.noreply.github.com>
2022-10-02 07:10:09 +0700

    Add files via upload

---
+++ duit.py
@@ -0,0 +1,9 @@
+brt = int(input("Berat: "))
+hrg = int (input("Harga: "))
+ongkos = int (input("Ongkos: "))
+uang = int (input("Duid: "))
+
+sisa = (uang-hrg*brt-ongkos*2)
+
+
+print("Sisa Duid Ibu :",sisa)
