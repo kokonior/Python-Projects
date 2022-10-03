fa5f0b38952ee0040d724ebd6722c40a5496ad43
agetta <agettaneo@gmail.com>
2022-10-03 19:11:17 +0700

    agetta.py

---
+++ agetta.py
@@ -0,0 +1,9 @@
+# Memasukkan Inputan tahun
+angka1 = input('Tulis angka pertama: ')
+angka2 = input('Tulis angka kedua: ')
+
+# Mengkonversi tahun lalu Menjumlahkannya
+sum = int(angka1) + int(angka2)
+
+# Menampilkan Hasil Penjumlahan tahun
+print('Hasil Penjumlahan {0} dan {1} adalah {2}'.format(angka1, angka2, sum))
