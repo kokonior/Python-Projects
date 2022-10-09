aa39af8dbbb15f4afeb19c9243281ae03d8e762f
Ahmad maulana yusuf <77580091+WiiAhmad@users.noreply.github.com>
2022-10-10 06:29:15 +0700

    Create tabelperkalian.py
    Create tabelperkalian.py

---
+++ tabelperkalian.py
@@ -0,0 +1,8 @@
+# Menginput Angka
+angka = int(input("Menampilkan Tabel Perkalian dari: "))
+ 
+# Menghitung 10 Kali dari Kisaran 1 sampai 10
+for i in range(1, 11):
+ 
+#Menampilkan Tabel Perkalian
+    print(angka, 'x', i, '=', angka*i)
