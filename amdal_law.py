22417836f337e2c9bed603fec4309ddaa590aaea
dimaswsss <mas.dimas2910@gmail.com>
2022-10-11 15:45:48 +0700

    Add program to illustrate amdal law

---
+++ amdal_law.py
@@ -0,0 +1,19 @@
+# import important package
+import numpy as np
+import matplotlib.pyplot as plt
+
+# calculate speed_up based b and s
+def amdal(b, s):
+    # code here
+    return 1 / ( 1 - b + ( b / s ) )
+
+# create range number with numpy
+x = np.arange(10, 1000)
+
+# use looping to implement amdal law
+b = 0.6
+y = [ amdal(b, n) for n in x ]
+
+# plot
+plt.plot(x, y)
+plt.show()
