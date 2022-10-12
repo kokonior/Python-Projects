56963a96057f0864b42c3744274f12c5617fa881
Ashish Kumar <114761660+Ashish16Kumar@users.noreply.github.com>
2022-10-12 19:27:29 +0530

    Create Rock,Paper,Scissor.py

---
+++ Rock,Paper,Scissor.py
@@ -0,0 +1,25 @@
+def get_winner(p1, p2):
+   if p1 == p2:
+       return "It's a tie!"
+   elif p1 == 'rock':
+       if p2 == 'scissors':
+           return "First player wins!"
+       else:
+           return "Second Player wins!"
+   elif p1 == 'scissors':
+       if p2 == 'paper':
+           return "First player win!"
+       else:
+           return"Second player wins!"
+   elif p1 == 'paper':
+       if p2 == 'rock':
+           return "First player wins!"
+       else:
+           return "Second player win!"
+   else:
+       return "Invalid input!"
+      
+player1 = input("First player: rock, paper or scissors: ")
+player2 = input("Second Player: rock, paper or scissors: ")
+ 
+print(get_winner(player1, player2))
