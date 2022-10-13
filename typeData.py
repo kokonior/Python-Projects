24a8e934020e8c405455408b0831d3de914b0907
Vanmoel Jr <vanmoeljr8@gmail.com>
2022-10-13 19:17:48 +0700

    Add file typeData.py via online update

---
+++ typeData.py
@@ -0,0 +1,27 @@
+x = str("Hello World") #str
+print(x,type(x)) 
+x = int(20) #int
+print(x,type(x)) 
+x = float(20.5)	#float
+print(x,type(x)) 
+x = complex(1j)	#complex
+print(x,type(x)) 
+x = list(("apple", "banana", "cherry"))	#list
+print(x,type(x)) 
+x = tuple(("apple", "banana", "cherry")) #tuple
+print(x,type(x)) 
+x = range(6) #range
+print(x,type(x)) 
+x = dict(name="John", age=36) #dict
+print(x,type(x)) 
+x = set(("apple", "banana", "cherry")) #set
+print(x,type(x)) 
+x = frozenset(("apple", "banana", "cherry")) #frozenset
+print(x,type(x)) 
+x = bool(5)	#bool
+print(x,type(x)) 
+x = bytes(5) #bytes
+print(x,type(x)) 
+x = bytearray(5) #bytearray
+print(x,type(x)) 
+x = memoryview(bytes(5)) #memoryview
