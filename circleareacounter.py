cb265f0c33671684887daed25fd4a42e36ee0c43
sayampradhan <112542130+sayampradhan@users.noreply.github.com>
2022-10-15 02:32:02 +0530

    Clean Code

--- circleareacounter.py
+++ circleareacounter.py
@@ -1,5 +1,5 @@
-print ("Calculate Area Of Circle")
+print("Calculate Area Of Circle")
 jari = input("Input Radius Circle : ")
 phi = 3.14
-luas =  phi * float(jari) * float(jari)
-print ("Area of circle is",luas)
+luas = phi * float(jari) * float(jari)
+print("Area of circle is", luas)
