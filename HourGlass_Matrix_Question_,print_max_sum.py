7673fb7246120c344a81688e8dec10ee4082288d
sayampradhan <112542130+sayampradhan@users.noreply.github.com>
2022-10-15 01:53:10 +0530

    Update HourGlass_Matrix_Question_,print_max_sum.py

--- HourGlass_Matrix_Question_,print_max_sum.py
+++ HourGlass_Matrix_Question_,print_max_sum.py
@@ -1,17 +1,22 @@
 def max_sum(grid):
-    mxsum=-99999999
+    mxsum = -99999999
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            if j+2 >= len(grid[i]) or i+2 >= len(grid[j]):
+            if j + 2 >= len(grid[i]) or i + 2 >= len(grid[j]):
                 continue
-            sum=(grid[i][j]+grid[i][j+1]+grid[i][j+2])+(grid[i+1][j+1])+(grid[i+2][j]+grid[i+2][j+1]+grid[i+2][j+2])
+            sum = (
+                (grid[i][j] + grid[i][j + 1] + grid[i][j + 2])
+                + (grid[i + 1][j + 1])
+                + (grid[i + 2][j] + grid[i + 2][j + 1] + grid[i + 2][j + 2])
+            )
             if sum > mxsum:
-                mxsum=sum
+                mxsum = sum
             else:
                 continue
     return mxsum
 
-grid=[]
+
+grid = []
 for i in range(6):
-    l=[int(i) for i in input().split()]
+    l = [int(i) for i in input().split()]
     grid.append(l)
