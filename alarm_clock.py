de0ad36b0662d0ed82752c7ba9a3ba3c613d54b1
sayampradhan <112542130+sayampradhan@users.noreply.github.com>
2022-10-15 02:19:27 +0530

    Clean Code

--- alarm_clock.py
+++ alarm_clock.py
@@ -1,9 +1,10 @@
-from datetime import datetime   
+from datetime import datetime
 from playsound import playsound
+
 alarm_time = input("Enter the time of alarm to be set:HH:MM:SS\n")
-alarm_hour=alarm_time[0:3]
-alarm_minute=alarm_time[3:5]
-alarm_seconds=alarm_time[6:8]
+alarm_hour = alarm_time[0:3]
+alarm_minute = alarm_time[3:5]
+alarm_seconds = alarm_time[6:8]
 alarm_period = alarm_time[9:20].upper()
 print("Setting up alarm..")
 while True:
@@ -12,10 +13,10 @@ while True:
     current_minute = now.strftime("%M")
     current_seconds = now.strftime("%S")
     current_period = now.strftime("%p")
-    if(alarm_period==current_period):
-        if(alarm_hour==current_hour):
-            if(alarm_minute==current_minute):
-                if(alarm_seconds==current_seconds):
+    if alarm_period == current_period:
+        if alarm_hour == current_hour:
+            if alarm_minute == current_minute:
+                if alarm_seconds == current_seconds:
                     print("Wake Up!")
-                    playsound('audio.mp3')
+                    playsound("audio.mp3")
                     break
