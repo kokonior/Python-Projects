2dc924a6714c130ba70044a9d804863f998770af
Akhdanmuafa <makhdanmuafa@gmail.com>
2022-10-15 00:40:07 +0700

    adding personalinfo program

---
+++ personalinformation.py
@@ -0,0 +1,6 @@
+firstName=input("What is your first name ? ")
+lastName=input("What is your last name ?")
+location=input("What is your location ?")
+age=input("What is your age ?")
+
+print("Hi "+firstName,lastName+ "!"+" Your location is " +location+" and you are "+age+" years old")
