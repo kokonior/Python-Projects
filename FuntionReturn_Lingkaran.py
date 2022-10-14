5e18691844e5ff02d28b41d6f7e1bda9eb9dc78a
sayampradhan <112542130+sayampradhan@users.noreply.github.com>
2022-10-14 23:53:13 +0530

    Clean Code

--- FuntionReturn_Lingkaran.py
+++ FuntionReturn_Lingkaran.py
@@ -1,9 +1,12 @@
 # LUAS LINGKARAN (RETURN)
 print("====LUAS JAJAR GENJANG====")
+
+
 def Luas_Lingkaran(r):
     return 3.14 * (r * r)
- 
-r = float(input('Masukan jari-jari lingkaran: '))
+
+
+r = float(input("Masukan jari-jari lingkaran: "))
 luas = Luas_Lingkaran(r)
- 
-print('Luas Lingkaran: {}'.format(luas))
+
+print("Luas Lingkaran: {}".format(luas))
