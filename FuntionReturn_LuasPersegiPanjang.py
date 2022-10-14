b6ddc7178d539f679df84ffcf481726c4c92a3a5
sayampradhan <112542130+sayampradhan@users.noreply.github.com>
2022-10-14 23:53:41 +0530

    Clean Code

--- FuntionReturn_LuasPersegiPanjang.py
+++ FuntionReturn_LuasPersegiPanjang.py
@@ -1,8 +1,11 @@
 print("====LUAS PERSEGI PANJANG====")
+
+
 def Luas_Persegi_Panjang():
     panjang = int(input("Masukan Panjang: "))
     lebar = int(input("Masukan Lebar: "))
-    return (panjang * lebar)
+    return panjang * lebar
+
 
 luas = Luas_Persegi_Panjang()
 print("Luas Persegi Panjang: {}".format(luas))
