5fd0a847c74df90c16f7593791fbcb6ac8607f32
sayampradhan <112542130+sayampradhan@users.noreply.github.com>
2022-10-15 02:25:51 +0530

    Clean Code

--- binarytodecimal.py
+++ binarytodecimal.py
@@ -2,7 +2,7 @@ binary_no = list(input("Input a binary number: "))
 value = 0
 
 for i in range(len(binary_no)):
-	digit = binary_no.pop()
-	if digit == '1':
-		value = value + pow(2, i)
+    digit = binary_no.pop()
+    if digit == "1":
+        value = value + pow(2, i)
 print("The decimal value of the number is", value)
