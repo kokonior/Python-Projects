e5aaa07244eba4b6520dd2e1a1d04f31b7745697
sayampradhan <112542130+sayampradhan@users.noreply.github.com>
2022-10-14 23:49:59 +0530

    Clean Code

--- DigitalClock.py
+++ DigitalClock.py
@@ -4,18 +4,18 @@ from tkinter.ttk import *
 from time import strftime
 
 root = Tk()
-root.title('Clock')
+root.title("Clock")
+
 
 def time():
-	string = strftime('%H:%M:%S %p')
-	lbl.config(text = string)
-	lbl.after(1000, time)
+    string = strftime("%H:%M:%S %p")
+    lbl.config(text=string)
+    lbl.after(1000, time)
 
-lbl = Label(root, font = ('calibri', 40, 'bold'),
-			background = 'purple',
-			foreground = 'white')
 
-lbl.pack(anchor = 'center')
+lbl = Label(root, font=("calibri", 40, "bold"), background="purple", foreground="white")
+
+lbl.pack(anchor="center")
 time()
 
 mainloop()
