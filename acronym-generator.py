daaa3911774e2c266f72ed76876ff5819329ad87
sayampradhan <112542130+sayampradhan@users.noreply.github.com>
2022-10-15 02:17:31 +0530

    Clean Code

--- acronym-generator.py
+++ acronym-generator.py
@@ -1,8 +1,8 @@
 user_input = input("Enter a phrase: ")
-phrase = (user_input.replace('of', '')).split()
+phrase = (user_input.replace("of", "")).split()
 acronym = ""
 
 for word in phrase:
     acronym = acronym + word[0].upper()
-    
-print(f'Acronym of {user_input} is {acronym}')
+
+print(f"Acronym of {user_input} is {acronym}")
