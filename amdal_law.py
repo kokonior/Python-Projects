8aed71e6c0af8b883baa263a10302b4f871d7e3d
sayampradhan <112542130+sayampradhan@users.noreply.github.com>
2022-10-15 02:20:03 +0530

    Clean Code

--- amdal_law.py
+++ amdal_law.py
@@ -5,14 +5,15 @@ import matplotlib.pyplot as plt
 # calculate speed_up based b and s
 def amdal(b, s):
     # code here
-    return 1 / ( 1 - b + ( b / s ) )
+    return 1 / (1 - b + (b / s))
+
 
 # create range number with numpy
 x = np.arange(10, 1000)
 
 # use looping to implement amdal law
 b = 0.6
-y = [ amdal(b, n) for n in x ]
+y = [amdal(b, n) for n in x]
 
 # plot
 plt.plot(x, y)
