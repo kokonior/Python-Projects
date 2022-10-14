5bc7c5f74fabcd4bae121ceb779108d53558aae4
sayampradhan <112542130+sayampradhan@users.noreply.github.com>
2022-10-15 02:29:17 +0530

    Clean Code

--- bubble_sortDescend.py
+++ bubble_sortDescend.py
@@ -1,7 +1,7 @@
 def bubble_sort(arr):
     # jumlah panjang list
     n = len(arr)
-    for i in range(n): 
+    for i in range(n):
         for j in range(n - i - 1):
             # membandingkan masing" elemen
             # jika ingin mengurutkan dari terbesar ganti "<" kalo dari terkecil ">"
@@ -9,6 +9,7 @@ def bubble_sort(arr):
                 # jika lebih besar, tukar.
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr
+
 
 unordered = [7, 5, 4, 9, 8, 2, 1, 6]
 print(bubble_sort(unordered))
