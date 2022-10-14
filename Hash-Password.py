7e1afa0ff1699fead759b27fd77ccd709f70a447
sayampradhan <112542130+sayampradhan@users.noreply.github.com>
2022-10-15 01:52:29 +0530

    Clean Code

--- Hash-Password.py
+++ Hash-Password.py
@@ -1,21 +1,26 @@
 import uuid
 import hashlib
 
+
 def hash_password(password):
-   salt = uuid.uuid4().hex
-   return hashlib.sha256(salt.encode() + password.encode()).hexdigest() + ':' + salt
+    salt = uuid.uuid4().hex
+    return hashlib.sha256(salt.encode() + password.encode()).hexdigest() + ":" + salt
+
 
 def check_password(hashed_password, user_password):
-   password, salt = hashed_password.split(':')
-   return password == hashlib.sha256(salt.encode() + user_password.encode()).hexdigest()
+    password, salt = hashed_password.split(":")
+    return (
+        password == hashlib.sha256(salt.encode() + user_password.encode()).hexdigest()
+    )
 
-new_pass = input('Please enter a password: ')
+
+new_pass = input("Please enter a password: ")
 hashed_password = hash_password(new_pass)
-print('The string to store in the db is: ' + hashed_password)
+print("The string to store in the db is: " + hashed_password)
 
-#Input your password
-old_pass = input('Now please enter the password again to check: ')
+# Input your password
+old_pass = input("Now please enter the password again to check: ")
 if check_password(hashed_password, old_pass):
-   print('You entered the right password')
+    print("You entered the right password")
 else:
-   print('Passwords do not match')
+    print("Passwords do not match")
