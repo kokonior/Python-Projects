7433099052140ab28a05f1fcf12e2f8e33f82ba9
sayampradhan <112542130+sayampradhan@users.noreply.github.com>
2022-10-15 02:46:52 +0530

    Clean Code

--- fractal_tree.py
+++ fractal_tree.py
@@ -1,34 +1,36 @@
 from turtle import *
 
-speed('fastest')
-  
+speed("fastest")
+
 rt(-90)
-  
+
 angle = 30
-  
-def draw(sz, level):   
-  
+
+
+def draw(sz, level):
+
     if level > 0:
         colormode(255)
-          
-        pencolor(0, 255//level, 0)
-          
+
+        pencolor(0, 255 // level, 0)
+
         fd(sz)
-  
+
         rt(angle)
 
-        draw(0.8 * sz, level-1)
-          
-        pencolor(0, 255//level, 0)
-          
-        lt(2 * angle )
-  
-        draw(0.8 * sz, level-1)
-          
-        pencolor(0, 255//level, 0)
-          
+        draw(0.8 * sz, level - 1)
+
+        pencolor(0, 255 // level, 0)
+
+        lt(2 * angle)
+
+        draw(0.8 * sz, level - 1)
+
+        pencolor(0, 255 // level, 0)
+
         rt(angle)
-        
+
         fd(-sz)
-           
+
+
 draw(80, 7)
