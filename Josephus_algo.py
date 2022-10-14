29c7e09501fec6c104c18fb1e5738c2ce27a673e
sayampradhan <112542130+sayampradhan@users.noreply.github.com>
2022-10-15 01:57:00 +0530

    Clean Code

--- Josephus_algo.py
+++ Josephus_algo.py
@@ -1,12 +1,11 @@
 def josephus(n, k):
 
-      if (n == 1):
-          return 0
-      else:
-    
-    
+    if n == 1:
+        return 0
+    else:
 
-          return (josephus(n - 1, k) + k-1) % n 
+        return (josephus(n - 1, k) + k - 1) % n
+
 
 # Driver Program to test above function
 n = 14
