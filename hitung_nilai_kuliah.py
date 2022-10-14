a7bf5a974802ca50db74ecdb6f758c997bf03261
sayampradhan <112542130+sayampradhan@users.noreply.github.com>
2022-10-15 02:51:04 +0530

    Clean Code

--- hitung_nilai_kuliah.py
+++ hitung_nilai_kuliah.py
@@ -1,30 +1,30 @@
-#Menginput Nilai Tugas, UTS, dan UAS
+# Menginput Nilai Tugas, UTS, dan UAS
 tugas = float(input("Masukkan nilai Tugas: "))
 uts = float(input("Masukkan nilai UTS: "))
 uas = float(input("Masukkan nilai UAS: "))
- 
-#Menghitung Nilai Akhir sesuai dengan Bobot
-nilai =  (0.15 * tugas) + (0.35 * uts) +  (0.50 * uas)
- 
-#Menentukan Grade Berdasarkan Nilai Akhir
+
+# Menghitung Nilai Akhir sesuai dengan Bobot
+nilai = (0.15 * tugas) + (0.35 * uts) + (0.50 * uas)
+
+# Menentukan Grade Berdasarkan Nilai Akhir
 if nilai > 80:
-    grade = 'A'
+    grade = "A"
 elif nilai > 70:
-    grade = 'B'
+    grade = "B"
 elif nilai > 60:
-    grade = 'C'
+    grade = "C"
 elif nilai > 50:
-    grade = 'D'
+    grade = "D"
 else:
-    grade = 'E'
- 
-#Menentukan Status Kelulusan Berdasarkan Nilai Akhir
+    grade = "E"
+
+# Menentukan Status Kelulusan Berdasarkan Nilai Akhir
 if nilai > 60:
-    status = 'Lulus'
+    status = "Lulus"
 else:
-    status = 'Tidak Lulus'
- 
-#Menampilkan Nilai Akhir, Grade, dan Status Kelulusan
-print('Nilai Akhir: %0.2f' % nilai)
-print('Grade: {}'.format(grade))
-print('Status: {}'.format(status))
+    status = "Tidak Lulus"
+
+# Menampilkan Nilai Akhir, Grade, dan Status Kelulusan
+print("Nilai Akhir: %0.2f" % nilai)
+print("Grade: {}".format(grade))
+print("Status: {}".format(status))
