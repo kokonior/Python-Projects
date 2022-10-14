9a1020849fee324a2500786267c67ad42f317bfb
sayampradhan <112542130+sayampradhan@users.noreply.github.com>
2022-10-15 02:34:26 +0530

    Clean Code

--- dadu2_1.py
+++ dadu2_1.py
@@ -1,6 +1,7 @@
 # program dua dadu v2 oleh NIM 030
 
 import random
+
 while True:
     lempar_1 = random.randint(1, 6)
     lempar_2 = random.randint(1, 6)
@@ -8,4 +9,4 @@ while True:
     print("Dadu no.2 = {}".format(lempar_1, lempar_2))
     if lempar_1 == 6 and lempar_2 == 6:
         break
-print('DOUBLE SIX !!!!!')
+print("DOUBLE SIX !!!!!")
