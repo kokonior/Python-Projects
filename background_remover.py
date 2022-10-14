781a073380fa2d83ef8f2976488340c307cf5800
sayampradhan <112542130+sayampradhan@users.noreply.github.com>
2022-10-15 02:23:18 +0530

    Clean Code

--- background_remover.py
+++ background_remover.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 # load image
-img = cv2.imread('person.png')
+img = cv2.imread("person.png")
 
 # convert to graky
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -15,16 +15,16 @@ mask = 255 - mask
 
 # apply morphology to remove isolated extraneous noise
 # use borderconstant of black since foreground touches the edges
-kernel = np.ones((3,3), np.uint8)
+kernel = np.ones((3, 3), np.uint8)
 mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
 mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
 
 # anti-alias the mask -- blur then stretch
 # blur alpha channel
-mask = cv2.GaussianBlur(mask, (0,0), sigmaX=2, sigmaY=2, borderType = cv2.BORDER_DEFAULT)
+mask = cv2.GaussianBlur(mask, (0, 0), sigmaX=2, sigmaY=2, borderType=cv2.BORDER_DEFAULT)
 
 # linear stretch so that 127.5 goes to 0, but 255 stays 255
-mask = (2*(mask.astype(np.float32))-255.0).clip(0,255).astype(np.uint8)
+mask = (2 * (mask.astype(np.float32)) - 255.0).clip(0, 255).astype(np.uint8)
 
 # put mask into alpha channel
 result = img.copy()
@@ -32,7 +32,7 @@ result = cv2.cvtColor(result, cv2.COLOR_BGR2BGRA)
 result[:, :, 3] = mask
 
 # save resulting masked image
-cv2.imwrite('person_transp_bckgrnd.png', result)
+cv2.imwrite("person_transp_bckgrnd.png", result)
 
 # display result, though it won't show transparency
 cv2.imshow("INPUT", img)
