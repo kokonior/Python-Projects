3e034d41ae21738c8896646d30aa5e9633dc6923
sayampradhan <112542130+sayampradhan@users.noreply.github.com>
2022-10-14 23:54:32 +0530

    Clean Code

--- Graphics_with_Python.py
+++ Graphics_with_Python.py
@@ -1,18 +1,19 @@
 import random
 import turtle
-colors = ['red','cyan','pink' ,'yellow', 'green','orange']
+
+colors = ["red", "cyan", "pink", "yellow", "green", "orange"]
 t = turtle.Turtle()
 t.speed(10)
 turtle.bgcolor("black")
-length=100
-angle =50
-size=5
+length = 100
+angle = 50
+size = 5
 for i in range(length):
-    color=random.choice(colors)
+    color = random.choice(colors)
     t.pencolor(color)
     t.fillcolor(color)
     t.penup()
-    t.forward(i+50)
+    t.forward(i + 50)
     t.pendown()
     t.left(angle)
     t.begin_fill()
