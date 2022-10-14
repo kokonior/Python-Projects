801fec559be916a1684772dd84a9235442e2137f
sayampradhan <112542130+sayampradhan@users.noreply.github.com>
2022-10-14 23:48:45 +0530

    Clean Code

--- Assistant.py
+++ Assistant.py
@@ -1,16 +1,16 @@
-import pyttsx3 
-import speech_recognition as sr 
+import pyttsx3
+import speech_recognition as sr
 import datetime
-import wikipedia 
+import wikipedia
 import webbrowser
 import os
 import smtplib
 
 
-engine = pyttsx3.init('sapi5')
-voices = engine.getProperty('voices')
+engine = pyttsx3.init("sapi5")
+voices = engine.getProperty("voices")
 # print(voices[1].id)
-engine.setProperty('voice', voices[1].id)
+engine.setProperty("voice", voices[1].id)
 
 
 def speak(audio):
@@ -20,19 +20,20 @@ def speak(audio):
 
 def wishMe():
     hour = int(datetime.datetime.now().hour)
-    if hour>=0 and hour<12:
+    if hour >= 0 and hour < 12:
         speak("Good Morning!")
 
-    elif hour>=12 and hour<18:
-        speak("Good Afternoon!")   
+    elif hour >= 12 and hour < 18:
+        speak("Good Afternoon!")
 
     else:
-        speak("Good Evening!")  
+        speak("Good Evening!")
 
-    speak("Welcome Master Yug. I am Alpha. Please tell me how may I help you ?")       
+    speak("Welcome Master Yug. I am Alpha. Please tell me how may I help you ?")
+
 
 def takeCommand():
-    #It takes microphone input from the user and returns string output
+    # It takes microphone input from the user and returns string output
 
     r = sr.Recognizer()
     with sr.Microphone() as source:
@@ -41,78 +42,77 @@ def takeCommand():
         audio = r.listen(source)
 
     try:
-        print("Recognizing...")    
-        query = r.recognize_google(audio, language='en-in')
+        print("Recognizing...")
+        query = r.recognize_google(audio, language="en-in")
         print(f"User said: {query}\n")
 
     except Exception as e:
-        # print(e)    
-        print("Say that again please...")  
+        # print(e)
+        print("Say that again please...")
         return "None"
     return query
 
+
 def sendEmail(to, content):
-    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server = smtplib.SMTP("smtp.gmail.com", 587)
     server.ehlo()
     server.starttls()
-    server.login('youremail@gmail.com', 'your-password')
-    server.sendmail('youremail@gmail.com', to, content)
+    server.login("youremail@gmail.com", "your-password")
+    server.sendmail("youremail@gmail.com", to, content)
     server.close()
+
 
 if __name__ == "__main__":
     wishMe()
-    #chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
+    # chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
     while True:
-    # if 1:
+        # if 1:
         query = takeCommand().lower()
-        #webbrowser.get(using='chrome')
+        # webbrowser.get(using='chrome')
         # Logic for executing tasks based on query
-        if 'wikipedia' in query:
-            speak('Searching Wikipedia...')
+        if "wikipedia" in query:
+            speak("Searching Wikipedia...")
             query = query.replace("wikipedia", "")
             results = wikipedia.summary(query, sentences=2)
             speak("According to Wikipedia")
             print(results)
             speak(results)
 
-        elif 'open youtube' in query:
-            speak('opening Youtube...')
+        elif "open youtube" in query:
+            speak("opening Youtube...")
             webbrowser.open("youtube.com")
 
-        elif 'open google' in query:
+        elif "open google" in query:
             webbrowser.open("google.com")
 
-        elif 'open stackoverflow' in query:
-            webbrowser.open("stackoverflow.com")   
+        elif "open stackoverflow" in query:
+            webbrowser.open("stackoverflow.com")
 
-
-        elif 'play music' in query:
-            music_dir = 'D:\\Non Critical\\songs\\Favorite Songs2'
+        elif "play music" in query:
+            music_dir = "D:\\Non Critical\\songs\\Favorite Songs2"
             songs = os.listdir(music_dir)
-            print(songs)    
+            print(songs)
             os.startfile(os.path.join(music_dir, songs[0]))
 
-        elif 'the time' in query:
-            strTime = datetime.datetime.now().strftime("%H:%M:%S")   
+        elif "the time" in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
             hour = int(datetime.datetime.now().hour)
             if hour >= 12:
-                hour = hour-12
+                hour = hour - 12
             minutes = int(datetime.datetime.now().min)
 
             speak(f"Sir, the time is {hour} hours {minutes} minutes")
 
-        elif 'your name' in query:
+        elif "your name" in query:
             speak(f"Sir, My name is Alpha")
 
-        
-
-        elif 'email to santosh' in query:
+        elif "email to santosh" in query:
             try:
                 speak("What should I say?")
                 content = takeCommand()
-                to = "yugsraval@gmail.com"    
+                to = "yugsraval@gmail.com"
                 sendEmail(to, content)
                 speak("Email has been sent!")
             except Exception as e:
                 print(e)
-                speak("Sorry my friend . I am not able to send this email")    
+                speak("Sorry my friend . I am not able to send this email")
