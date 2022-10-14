dc1868f4a68f03376ace969ec3b9f8f88b50c394
sayampradhan <112542130+sayampradhan@users.noreply.github.com>
2022-10-15 02:20:49 +0530

    Clean Code

--- ammarsufyan_caturBenteng.py
+++ ammarsufyan_caturBenteng.py
@@ -1,17 +1,17 @@
-ukuran_catur = int(input('Masukkan Ukuran Catur : '))
+ukuran_catur = int(input("Masukkan Ukuran Catur : "))
 list2D = []
 
-#Papan Catur
-print('Masukkan papan catur:')
+# Papan Catur
+print("Masukkan papan catur:")
 for n in range(ukuran_catur):
     isi_string = input()
     isi_list = isi_string.split()
     list2D.append(isi_list)
 
-#Posisi Benteng
+# Posisi Benteng
 for i in range(ukuran_catur):
     for j in range(ukuran_catur):
-        if list2D[i][j] == 'R':
+        if list2D[i][j] == "R":
             posisi_X = i
             posisi_Y = j
 
@@ -19,15 +19,15 @@ hasil = 0
 
 try:
     for s in range(len(list2D)):
-        if list2D[posisi_X+s][posisi_Y] == 'P':
+        if list2D[posisi_X + s][posisi_Y] == "P":
             hasil += 1
-        if list2D[posisi_X][posisi_Y+s] == 'P':
-            hasil += 1    
-        if list2D[posisi_X-s][posisi_Y] == 'P':
-            if posisi_X-s >= 0:
+        if list2D[posisi_X][posisi_Y + s] == "P":
+            hasil += 1
+        if list2D[posisi_X - s][posisi_Y] == "P":
+            if posisi_X - s >= 0:
                 hasil += 1
-        if list2D[posisi_X][posisi_Y-s] == 'P':
-            if posisi_Y-s >= 0:
+        if list2D[posisi_X][posisi_Y - s] == "P":
+            if posisi_Y - s >= 0:
                 hasil += 1
 except IndexError:
     pass
