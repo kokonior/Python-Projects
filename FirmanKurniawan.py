25b23c8fe773bf5759c09d65f11d46da1e1c8549
sayampradhan <112542130+sayampradhan@users.noreply.github.com>
2022-10-14 23:51:30 +0530

    Clean Code

--- FirmanKurniawan.py
+++ FirmanKurniawan.py
@@ -1,16 +1,21 @@
 def kalkulator(pilih, angka1, angka2):
-    if(pilih == "1"):
+    if pilih == "1":
         tambah = angka1 + angka2
         print(tambah)
-    elif(pilih == "2"):
+    elif pilih == "2":
         kurang = angka1 - angka2
         print(kurang)
-    elif(pilih == "3"):
+    elif pilih == "3":
         kali = angka1 * angka2
         print(kali)
     else:
         bagi = angka1 / angka2
         print(bagi)
 
+
 print("1.Tambah \n2.Kurang\n3.Kali\n4.Bagi")
-kalkulator(input("Masukkan Pilihan (1-4): "),int(input("Masukkan angka pertama: ")),int(input("Masukkan angka kedua: ")))
+kalkulator(
+    input("Masukkan Pilihan (1-4): "),
+    int(input("Masukkan angka pertama: ")),
+    int(input("Masukkan angka kedua: ")),
+)
