147b065bf8bd619ed22f711ec3370c7032f704b1
sayampradhan <112542130+sayampradhan@users.noreply.github.com>
2022-10-15 02:39:19 +0530

    Clean Code

--- ejemplo.py
+++ ejemplo.py
@@ -7,15 +7,16 @@ Created on Tue May 24 14:33:07 2022
 
 import cv2
 from matplotlib import pyplot as plt
+
 # Lectura de imagen
-#imagen = cv2.imread('Material/OpenCV_logo.png')
+# imagen = cv2.imread('Material/OpenCV_logo.png')
 # Leer en escala de grises -> imagen = cv2.imread('Material/OpenCV_logo.png')
-imagen = cv2.imread('Material/retrato.jpg', 0)
+imagen = cv2.imread("Material/retrato.jpg", 0)
 imagen = cv2.equalizeHist(imagen)
 
 # Imagen a color
-imagenC = cv2.imread('Material/retrato.jpg')
-'''
+imagenC = cv2.imread("Material/retrato.jpg")
+"""
 # Crear un histograma
 hist = cv2.calcHist(imagen, [0], None, [256], [0,256])
 # Graficar el histograma
@@ -23,32 +24,23 @@ plt.plot(hist, color = 'gray')
 plt.xlabel('Intensidad de iluminación')
 plt.ylabel('Canidad de pixeles')
 plt.show()
-'''
+"""
 
-# Histograma a color 
-color = ('b','g','r')
+# Histograma a color
+color = ("b", "g", "r")
 # Lee c como los datos para una variable con bgr
-for i,c in enumerate(color):
-    hist = cv2.calcHist(imagenC, [i], None, [256], [0,256])
-    plt.plot (hist, color = c)
-    plt.xlim(0,256)
+for i, c in enumerate(color):
+    hist = cv2.calcHist(imagenC, [i], None, [256], [0, 256])
+    plt.plot(hist, color=c)
+    plt.xlim(0, 256)
 plt.show()
-'''
+"""
 # Guardar la imagen
 cv2.imwrite('Material/opencv_1.png', imagen)
-'''
+"""
 
 # Mostrar la imagen
-cv2.imshow('Logo opencv', imagenC)
-#Espera a que presionemos la tecla
+cv2.imshow("Logo opencv", imagenC)
+# Espera a que presionemos la tecla
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
-
-
-
-
-
-
-
