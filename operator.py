c0e03d3d65dc79e304be5220b9ef493b5c9cdcf2
Vanmoel Jr <vanmoeljr8@gmail.com>
2022-10-14 09:08:05 +0700

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,16 @@
+x = 5	# mean x = 5
+print(x)
+x += 3	# mean x = x + 3
+print(x)
+x -= 3	# mean x = x - 3
+print(x)
+x *= 3	# mean x = x * 3
+print(x)
+x /= 3	# mean x = x / 3
+print(x)
+x %= 3	# mean x = x % 3
+print(x)
+x //= 3	# mean x = x // 3
+print(x)
+x **= 3	# mean x = x ** 3
+print(x)
