e4908b0deaedb275f18795bdd8d28889b1d282c4
sayampradhan <112542130+sayampradhan@users.noreply.github.com>
2022-10-15 02:53:14 +0530

    Update joni4.py

--- joni4.py
+++ joni4.py
@@ -1,13 +1,16 @@
 # import required modules
 import pandas as pd
 import numpy as np
- 
+
 # create dataset
-df = pd.DataFrame({'Temperature': ['Hot', 'Cold', 'Warm', 'Cold'],
-                   })
- 
+df = pd.DataFrame(
+    {
+        "Temperature": ["Hot", "Cold", "Warm", "Cold"],
+    }
+)
+
 # display dataset
 print(df)
- 
+
 # create dymmy variables
 pd.get_dummies(df)
