73a90def99ed7f59ccfa4852f4438fbf7327a691
sayampradhan <112542130+sayampradhan@users.noreply.github.com>
2022-10-15 01:55:25 +0530

    Clean Code

--- Internet_Speed_Check.py
+++ Internet_Speed_Check.py
@@ -1,6 +1,7 @@
 import speedtest
+
 speed = speedtest.Speedtest()
 download_speed = speed.download()
 upload_speed = speed.upload()
-print(f'The download speed is {download_speed}')
-print(f'The uplaod speed is {upload_speed}')                                  
+print(f"The download speed is {download_speed}")
+print(f"The uplaod speed is {upload_speed}")
