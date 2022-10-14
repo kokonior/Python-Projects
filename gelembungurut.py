5cbcc589b88b5df94674bc05000505d28e5641e5
sayampradhan <112542130+sayampradhan@users.noreply.github.com>
2022-10-15 02:47:24 +0530

    Clean Code

--- gelembungurut.py
+++ gelembungurut.py
@@ -1,10 +1,11 @@
 def bubble_sort(angka: list) -> list:
     for item in angka:
-        for i in range(len(angka)-1):
-            if angka[i] > angka[i+1]:
-                angka[i], angka[i+1] = angka[i+1], angka[i]
+        for i in range(len(angka) - 1):
+            if angka[i] > angka[i + 1]:
+                angka[i], angka[i + 1] = angka[i + 1], angka[i]
+
 
 number = [6, 5, 3, 1, 8, 7, 2, 4]
 bubble_sort(number)
 print(number)
-print('HACKTOBERFEST')
+print("HACKTOBERFEST")
